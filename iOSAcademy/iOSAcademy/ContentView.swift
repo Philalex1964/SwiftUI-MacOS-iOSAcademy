b8d76f9d5c83d88653777503d6367889ee283311
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-05 11:21:22 +0200

    created side menu and mainView

--- iOSAcademy/iOSAcademy/ContentView.swift
+++ iOSAcademy/iOSAcademy/ContentView.swift
@@ -7,10 +7,78 @@
 
 import SwiftUI
 
+struct Option: Hashable {
+    let title: String
+    let imageName: String
+}
+
 struct ContentView: View {
+    let options: [Option] = [
+        .init(title: "Home", imageName: "house"),
+        .init(title: "About", imageName: "info.circle"),
+        .init(title: "Settings", imageName: "gear"),
+        .init(title: "Social", imageName: "message")
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            ListView(options: options)
+            
+            MainView()
+        }
+        .frame(minWidth: 600, minHeight: 400)
+    }
+}
+
+struct ListView: View {
+    let options: [Option]
+    
+    var body: some View {
+        VStack {
+            ForEach(options, id: \.self) { option in
+                HStack {
+                    Image(systemName: option.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20)
+                    
+                    Text(option.title)
+                    
+                    Spacer()
+                }
+            }
             .padding()
+        }
+        
+        Spacer()
+    }
+}
+
+struct MainView: View {
+    let cols: [GridItem] = [
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible())
+    ]
+    
+    let videoImages = Array(1...6).map { "video\($0)" }
+    
+    var body: some View {
+        VStack {
+            Image("header")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            LazyVGrid(columns: cols) {
+                ForEach(videoImages, id: \.self) { imageName in
+                    Image(imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+            }
+            
+            Spacer()
+        }
     }
 }
 
